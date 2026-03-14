37c33c3b44446f61aa8ac82d35c763a976bf8ead
Bright Emmanuel <brighto7700@gmail.com>
2026-03-14 11:38:52 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import createMDX from "@next/mdx";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+};
+
+const withMDX = createMDX({});
+export default withMDX(nextConfig);
